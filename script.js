2885df102ac8b8ab2625000d896021552455ad12
Ian Wren <ian.wren81@gmail.com>
2020-09-28 12:16:34 -0700

    Added functions and vairables to populate text for questions in quiz, arrays for questions, and possible answers.

--- script.js
+++ script.js
@@ -6,10 +6,18 @@ let answerOne = document.querySelector("#a1");
 let answerTwo = document.querySelector("#a2");
 let answerThree = document.querySelector("#a3");
 let answerFour = document.querySelector("#a4");
+let questionsEl = document.querySelector("#Questions");
 
-function getStarted(){
-  let questionEl.textContent("Do you want to start the quiz?")
-}
+let questionOne = ["Question 1?","Answer 1","Answer 2","Answer 3","Answer 4"]
+let questionTwo = ["Question 2?","Answer 1","Answer 2", "Answer 3","Answer 4"];
+let questionThree = ["Question 3?","Answer 1","Answer 2","Answer 3","Answer 4"];
+let questionFour = ["Question 4?","Answer 1","Answer 2","Answer 3","Answer 4"];
+let questionFive = ["Question 5?","Answer 1","Answer 2","Answer 3","Answer 4"];
+let questionSix = ["Question 6?","Answer 1","Answer 2","Answer 3","Answer 4"];
+
+let questions = [questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix];
+
+
 
 
 
@@ -27,30 +35,40 @@ function startQuiz() {
         highscore();
       }
     }, 1000);
-    function buttonFill(){
-      
-    }
+    function buttonFill(answers){
+      for (i=1; i < answers.length; i++)
+      questionsEl.children[i].children[0].textContent = answer[i];
+     };
+    function questionFill(question){
+      for (i=0; i= 1; i++)
+      questionsEl.children[i].textContent = question[i];
+    };
  
- 
-    function askQ(){
+    function askQ()
+    {
     function buttonFill();
     function questionFill();
+    answerOne.addEventListener("click", function () {});
+    answerTwo.addEventListener("click", function () {});
+    answerThree.addEventListener("click", function () {});
+    answerFour.addEventListener("click", function () {});
 
   };
 
   for (var i = 0; i < questions.length; i++) {
-    // Display current question to user and ask OK/Cancel
+    // Display current question to user and wait for answer on buttons
     function askQ();
 
     // Compare answers
-    if ((answer === true && questions[i].a === "t") ||
-      (answer === false && questions[i].a === "f")) {
+    if ((answer === true && questions[i].a === "t"))
+       {
       // Increase score
       score++;
       alert("Correct!");
     }
     else {
       alert("Wrong!");
+      timerLeft-5;
     }
   }
 
